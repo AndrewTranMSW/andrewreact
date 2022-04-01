131bb59ab8f94a49de8d2538c92c5155d55190ce
Andrew Tran <andrewtranmsw@gmail.com>
2022-04-01 03:00:22 -0700

    done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,8 +8,8 @@ export default function Header() {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h2>Andrew Tran</h2>
+        <h4>Hello I'm</h4>
+        <h1>Andrew Tran</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
         <HeaderSocials />
